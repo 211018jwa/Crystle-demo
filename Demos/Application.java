290dd66bc78dfae53d75948890336d7dc4fc01c2
NinjaBuddyYin <ninjabuddyyin@gmail.com>
2021-10-20 11:48:25 -0400

    Demo Test

---
+++ Demos/Application.java
@@ -0,0 +1,14 @@
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        myOwnMethod();
+        System.out.println("Here is code that" + "I am writing with notepad");
+        System.out.println("End of main method");
+    }
+
+    public static void myOwnMethod() {
+        System.out.println("myOwnMethod executing()");
+    }
+
+}
